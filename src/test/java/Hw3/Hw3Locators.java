fd17fd64d445434de638a6be336550dd25c6716c
AnniLovc <lrvcom@inbox.lv>
2022-11-15 10:55:49 +0200

    HomeWork 3/Find out Locators on Discovercars.com

---
+++ src/test/java/Hw3/Hw3Locators.java
@@ -0,0 +1,42 @@
+package Hw3;
+
+public class Hw3Locators {
+    //----------FIRST PAGE-----------------------
+
+    //*[contains (@class, 'logo-cell')]
+    //*[contains (@class, 'header static-index')]/div
+    //a [@id = 'currentCurrency']
+    //a [@data-label = 'Navigation']
+    //div [@class = 'home-caption']
+    //div[@class= 'fake-field clearfix']
+    //span[@class = 'sb-residence-text']
+    //*[@id='location-submit']
+    //*[contains(@class, 'arrow-slider-circle')]
+    //div[@class = 'tp-widget-review']
+    //*[contains(@class,  'row is-table-row')]
+
+    //-----------PAGE WITH CARS-------------------
+
+    //div [@data-id = "car-groups-small"]
+    //div [@class = 'car-top-filter-control next']
+    //span [@class = 'showing-cars']
+    //a [@class = 'dc-ui dropdown-toggle text-blue-500']
+    //ul [@class = 'dc-list dc-list-icon dc-list-md text-14 dc-list-horizontal dc-list-mt-8 car-params mt-8 text-gray-500']/li/span - ne doshla do elementa
+    //div [@class= 'flt-type']
+    //div [@class= 'rc-link']/a
+    //a [@class= 'btn btn-book submit click-on-car selected-car prevent-loading instant']
+    //span [@class= 'min-price']
+    //* [contains(@data-text, 'Gasoline')]
+    //label[@for = 'number-of-seats-seats-4']
+    //div[@class= 'supplier-rating']
+    //div[@class= 'price-item-price-main']
+
+
+
+    //-----------------TASK WITH *------------------NE SDELANO DO KONCA!
+    //body[@data-page= 'search-index']
+
+    //div [@class= 'lds-ellipsis v2']
+    //div [@class= 'mb-8 text-24 text-bold text-center']
+    //div [@class= 'mb-24 text-center']
+}
